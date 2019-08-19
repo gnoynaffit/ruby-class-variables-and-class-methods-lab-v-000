b36e82a391dea253617c969f9a9a571e7eaed0bd
Tiffany <ong.tifffany1503@gmail.com>
2019-08-19 17:45:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,13 +27,13 @@ class Song
   end
 
   def self.genres_count
-    genres_count = Hash.new(0)
+    genres_count = Hash.new(1)
     @@genres.each {|genre| genres_count[genres] =+ 1}
     genre_count
     end
 
   def self.artist_count
-    artist_count = Hash.new(0)
+    artist_count = Hash.new(1)
     @@artists.each {|artist| artist_count[artist] =+ 1}
     artist_count
   end
