eb71a68155e0081af03d9126334a1fdc849f5293
Tiffany <ong.tifffany1503@gmail.com>
2019-08-19 17:37:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@artists = []
-  @@grenres = []
+  @@genres = []
 
   def initialize(song_name,artist,genre)
     @name = song_name
