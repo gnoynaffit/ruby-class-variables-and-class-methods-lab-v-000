dc4cc9af976956c9ee2892f06dd1c8f69a4c4130
Tiffany <ong.tifffany1503@gmail.com>
2019-08-19 17:52:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,17 +30,24 @@ class Song
     genres_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
+        genre_count[genre] += 1
       else
-        genre_count[genre]
+        genre_count[genre] += 1
       end
     end
     genre_count
     end
 
   def self.artist_count
-    artist_count = Hash.new(0)
-    @@artists.each {|artist| artist_count[artist] =+ 1}
-    artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] =+ 1
+    else
+      artist_count[artist] = 1
+    end
+  end
+  artist_count
   end
 
 end
